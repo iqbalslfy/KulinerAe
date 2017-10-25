ad824140cb6e3b1ff9631098681c0aae001cb7ff
DESKTOP-NA8JGMG\SONY <muhammad.iqbal04998@gmail.com>
2017-10-25 15:57:53 +0700

    update

---
+++ app/src/main/java/net/kulinerae/kulinerae/Fragment/FragmentDua.java
@@ -0,0 +1,29 @@
+package net.kulinerae.kulinerae.Fragment;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import net.kulinerae.kulinerae.R;
+
+public class FragmentDua extends Fragment {
+
+    public FragmentDua() {
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_dua, container, false);
+    }
+
+}
---
+++ app/src/main/java/net/kulinerae/kulinerae/Fragment/FragmentTiga.java
@@ -0,0 +1,28 @@
+package net.kulinerae.kulinerae.Fragment;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import net.kulinerae.kulinerae.R;
+
+public class FragmentTiga extends Fragment {
+
+    public FragmentTiga() {
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_tiga, container, false);
+    }
+}
